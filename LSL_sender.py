9d96f8592be2cfddc6d30db03a71047c029be490
greg1877 <glbales@ucdavis.edu>
2021-12-10 10:38:59 -0800

    added clock to sender

--- LSL_sender.py
+++ LSL_sender.py
@@ -1,22 +1,32 @@
 import time
+import numpy as np
 from random import random as rand
 from pylsl import StreamInfo, StreamOutlet
 
-info1 = StreamInfo('BioSemi_1', 'EEG', 20, 100, 'float32', 'myuid67890')
-#info2 = StreamInfo('BioSemi_2', 'EEG', 20, 100, 'float32', 'myuidddd')
-#info3 = StreamInfo('BioSemi_3', 'Biophys', 20, 100, 'float32', 'myuidddd')
+# First Stream
+info1 = StreamInfo('Pseudo Noise', 'EEG', 20, 100, 'float32', 'myuid123')
 outlet1 = StreamOutlet(info1)
-#outlet2 = StreamOutlet(info2)
-#outlet3 = StreamOutlet(info3)
+
+# Second Stream
+info2 = StreamInfo('Local_Minute', 'Minute', 1, 100, 'float32', 'myuid456')
+outlet2 = StreamOutlet(info2)
+
+# Third Stream
+info3 = StreamInfo('Local_Second', 'Second', 1, 100, 'float32', 'myuid789')
+outlet3 = StreamOutlet(info3)
 
 print("now sending data...")
 try:
     while True:
-        mysample = [rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(),
+        mysample1 = [rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(),
                     rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand()]
-        outlet1.push_sample(mysample)
-        #outlet2.push_sample(mysample)
-        #outlet3.push_sample(mysample)
+        # Change the other "mysampleX" lists as desired
+        mysample2 = [time.localtime().tm_min]
+        mysample3 = [time.localtime().tm_sec]
+
+        outlet1.push_sample(mysample1)
+        outlet2.push_sample(mysample2)
+        outlet3.push_sample(mysample3)
         time.sleep(0.01)
 except KeyboardInterrupt:
     pass
